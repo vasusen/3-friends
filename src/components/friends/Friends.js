160b3d094d2b7ee59e965856573e33ed498e0329
vasusen <vasusen@gmail.com>
2018-05-01 21:44:15 -0700

    added cards based on mdbootstrap

--- src/components/friends/Friends.js
+++ src/components/friends/Friends.js
@@ -15,7 +15,7 @@ class Friends extends React.Component {
     this.setState({ loading: true });
 
     // get 3 random profiles from uinames.com
-    fetch('https://uinames.com/api/?amount=3')
+    fetch('https://uinames.com/api/?amount=3&ext')
     .then(response => {
       return response.json().then(json => {
         return response.ok ? json : Promise.reject(json);
@@ -51,12 +51,31 @@ class Friends extends React.Component {
 
     return (
       <div>
-        <h2>Three friends met...</h2>
-
+        <h2 class="mt-4 mb-4">Three friends met...</h2>
+        <div class="row">
+        <div class="col-md-8 mx-auto">
+        <div class="row">
         {profiles.map((profile, i) => (
-          <div key={i}>{profile.name} from {profile.region}</div>
+          
+            <div key={i} class="col-md-3 offset-md-1">
+            <div class="card">
+
+              <div class="text-center">
+                <img class="circle-img" src={profile.photo} alt="Profile phot" />
+              </div>
+
+              <div class="card-body">
+                <h4 class="card-title"><a>{profile.name} {profile.surname}</a></h4>
+                <p class="card-text">from {profile.region}</p>
+
+              </div>
+            </div>
+            </div>
         ))}
-      
+        </div>
+        </div>
+        </div>
+
       </div>
     );
   }
